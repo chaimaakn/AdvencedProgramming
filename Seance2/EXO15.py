a00f6c376f8d486607dbff077985d05053cbca07
Souha Aknouche <aknouchechaima00@gmail.com>
2024-12-11 17:03:10 +0100

    .

---
+++ Seance2/EXO15.py
@@ -0,0 +1,6 @@
+chaine = input("Please type in a string: ")
+
+
+print("a", "found" if "a" in chaine else "not found")
+print("e", "found" if "e" in chaine else "not found")
+print("o", "found" if "o" in chaine else "not found")
